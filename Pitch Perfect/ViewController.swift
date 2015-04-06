2d118da01239be1f70e230d915ca478360dfc74b
Michael Owens <mowens@ucdavis.edu>
2015-04-05 20:53:03 -0700

    Stop button now hides itself and 'recording' text when pushed

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
     
+    @IBOutlet weak var stopRecordAudio: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,14 +23,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        //hide stop button
+        stopRecordAudio.hidden = true
+    }
+    
     @IBAction func recordAudio(sender: UIButton) {
         //TODO: Record user's voice
         recordingInProgress.hidden = false
+        stopRecordAudio.hidden = false
     }
     
     @IBAction func stopRecordAudio(sender: UIButton) {
         recordingInProgress.hidden = true
+        stopRecordAudio.hidden = true
     }
 
 }

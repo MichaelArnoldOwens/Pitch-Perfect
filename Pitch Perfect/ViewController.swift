8434c1f6118ca59af7db0583973909ec00c5a840
Michael Owens <mowens@ucdavis.edu>
2015-04-29 15:32:45 -0700

    disables microphone button on tap and re-enables on tapping stop

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var recordingInProgress: UILabel!
-    
     @IBOutlet weak var stopRecordAudio: UIButton!
     
     override func viewDidLoad() {
@@ -25,19 +25,27 @@ class ViewController: UIViewController {
     }
     
     override func viewWillAppear(animated: Bool) {
+        //sets background color to blue
+        self.view.backgroundColor = UIColor.blueColor()
+
         //hide stop button
         stopRecordAudio.hidden = true
+
     }
     
     @IBAction func recordAudio(sender: UIButton) {
         //TODO: Record user's voice
         recordingInProgress.hidden = false
         stopRecordAudio.hidden = false
+        recordButton.enabled = false
     }
     
     @IBAction func stopRecordAudio(sender: UIButton) {
         recordingInProgress.hidden = true
         stopRecordAudio.hidden = true
+        recordButton.enabled = true
+
+        
     }
 
 }
